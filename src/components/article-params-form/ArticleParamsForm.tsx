7fa1b0babd132974a7eab626a6972d207f936923
Nick Boickov <nick.boickov@yandex.ru>
2025-11-27 11:00:22 +0300

    PR10.1-form

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,63 @@
+import { useState } from 'react';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Text } from 'src/ui/text';
 
 import styles from './ArticleParamsForm.module.scss';
+import { Select } from 'src/ui/select';
+import {
+	backgroundColors,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleToggle = () => {
+		setIsOpen(!isOpen);
+	};
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+	};
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
+			<ArrowButton isOpen={isOpen} onClick={handleToggle} />
 			<aside className={styles.container}>
-				<form className={styles.form}>
+				<form className={styles.form} onSubmit={handleSubmit}>
+					<Text size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Select
+						options={fontFamilyOptions}
+						selected={newFontFamilyOptions}
+						title='шрифт'
+						onChange={setNewFontFamilyOption}
+					/>
+					<RadioGroup
+						selected={newFontSize}
+						name='radio'
+						onChange={setNewFontSize}
+						options={fontSizeOptions}
+						title='размер шрифта'
+					/>
+					<Select
+						options={fontColors}
+						selected={newFontColor}
+						title='цвет шрифта'
+						onChange={setNewFontColor}
+					/>
+					<Separator />
+					<Select
+						options={backgroundColors}
+						selected={newBackgroundColor}
+						title='цвет фона'
+						onChange={setNewBackgroundColor}
+					/>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
